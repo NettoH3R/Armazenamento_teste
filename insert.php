aaaa56fee57df5fca578145a7e1a98664a418c64
João Netto <jovinettonetto@gmail.com>
2023-07-31 17:46:59 +0000

    Pronto

--- insert.php
+++ insert.php
@@ -2,8 +2,7 @@
 if (isset($_FILES['arquivo'])) {
     $file = $_FILES['arquivo'];
 
-    
-    
+    $nome = $_POST['nome'];
 
     // Verificar o tipo MIME do arquivo
     $allowedTypes = array('audio/mpeg', 'audio/mp3');
@@ -12,9 +11,25 @@ if (isset($_FILES['arquivo'])) {
     if (in_array($fileType, $allowedTypes)) {
         // O arquivo é um MP3 válido, faça o processamento aqui
         // ...
+        // var_dump($file);
+        
+        $pasta = 'assets/arquivos/';
+        
+        $nome = $_POST['nome'];
+        $extencao = $file['name'];
+        $extencao = strtolower(pathinfo($extencao, PATHINFO_EXTENSION));
+        $path = $pasta . $nome . "." . $extencao;
+        echo $extencao;
 
-        $file['name'] = $_POST['nome'];
-        var_dump($file);
+        //Envia o arquivo para a pasta selecionada
+        $deu_certo = move_uploaded_file($file['tmp_name'], $path);
+
+        if($deu_certo){
+            echo "Arquivo enviado com secesso <a href=\"assets/arquivos/$nome.$extencao\">Click </a>";
+        }else{
+            echo "<p>Falha ao Enviar o arquivo</p>";
+        }
+
     } else {
         // O arquivo não é um MP3 válido, exiba uma mensagem de erro
         echo "Tipo de arquivo inválido. Por favor, selecione um arquivo MP3.";
@@ -27,7 +42,7 @@ include('./includes/header.php') ?>
 <form action="./insert.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="arquivo">
     <br>
-    <table>Digite o neme da música:</table>
+    <table>Digite Nome da música:</table>
     <input type="text" name="nome">
     <br>
     <input type="submit" value="Enviar">
